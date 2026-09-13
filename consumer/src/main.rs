85d32d1e93846ca1e165dd5ae51f93b4deebe471
taylor <taylor@tfourteen.local>
2026-09-13 17:29:44 -0600

    stash rust consumer

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,12 +1,19 @@
-use eventsource_stream::Eventsource;
-use futures_util::stream::StreamExt;
-use reqwest::Client;
 use log::{error, info};
+use futures_util::stream::StreamExt;
+use eventsource_stream::Eventsource;
+use reqwest::Client;
+
+use rdkafka::config::ClientConfig;
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::Consumer;
+use rdkafka::message::{BorrowedMessage, OwnedMessage};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::Message;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    
+
     // URL for Wikimedia's real-time recent changes stream
     let url = "https://stream.wikimedia.org/v2/stream/recentchange";
     let client = Client::builder()
@@ -24,6 +31,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Convert the response body into an SSE stream
     let mut event_stream = response.bytes_stream().eventsource();
+
+    // Init producer
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", &brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
 
     // Iterate over incoming events asynchronously
     while let Some(event) = event_stream.next().await {
